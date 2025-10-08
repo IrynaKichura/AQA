88993ab716122b306162a491cc5b797e125d4196
IrynaKichura <Iryna.Kichura@gmail.com>
2025-10-08 20:52:50 +0300

    Added file: lesson3/nodeBasics/index.js

---
+++ lesson3/test.js
@@ -0,0 +1,28 @@
+//console.log("hello");
+//const variable = 'hello';
+//console.log(typeof variable);
+//console.log(variable);
+
+//import chalk from 'chalk';
+//console.log(chalk.blue('Hello world!'));
+/*
+const variable2 = 'hi';
+let variable3 = 'hey';
+variable3 = 'hey6';
+console.log(variable3);
+function hi() {
+    const variable2 = 'hi';
+let variable3 = 'hey';
+variable3 = 'hey6';
+return variable3;
+
+};
+console.log(variable2);
+*/
+
+//console.log("1" ===1 );//check for value and type
+
+function sayHi() {
+    return "hi";
+}
+
