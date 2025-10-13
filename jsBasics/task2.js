f12d8fd4c107177c7d7ab1f5d5a3226146f0455a
IrynaKichura <Iryna.Kichura@gmail.com>
2025-10-13 16:21:51 +0300

    Added task2

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+let name1 = 'Iryna';
+let name2 = 'Roman';
+
+let greeting1 = 'Hello ' + name1 + ' and hello ' + name2;
+console.log(greeting1);
+
+let greeting2 = `Hello ${name1} and hello ${name2}`;
+console.log(greeting2);     
