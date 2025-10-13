1ea5e2af29daf5c8a0def588e6aea0069c6ff86f
IrynaKichura <Iryna.Kichura@gmail.com>
2025-10-13 17:01:21 +0300

    Updated Task4_3

--- jsBasics/task4_3.js
+++ jsBasics/task4_3.js
@@ -1,5 +1,5 @@
 const radius = 3;
 const height = 6;
 const pi = Math.PI;
-let volume = pi * Math.pow(pi*radius, 2) * height;
+let volume = pi * Math.pow(radius, 2) * height;
 console.log('Volume is: ' + volume.toFixed(2));
