88642b1323ce80e7ca0901a5850130f591146026
IrynaKichura <Iryna.Kichura@gmail.com>
2025-10-13 19:39:48 +0300

    Added missed files

---
+++ lesson4/numbers.js
@@ -0,0 +1,35 @@
+const number = 42 + 0.7;
+let math=Math.PI
+math=Math.sqrt(16);
+math = Math.floor(3.9);
+
+math = Math.random();
+math = Math.random() * 10 ;
+math = (Math.random() * 10) + 5; // from 5 to 15
+math = Math.min(3,5,1,7,9,-2);
+
+let name = 'Iryna';
+let greeting = `Hello ${name}`;
+greeting=`Hello ${name}`;
+let lastName = 'Kichura';
+let fullName = name + ' ' + lastName;
+greeting=`Hello, I am ${name} and my last name is ${lastName}. I am ${number} years old.`;
+let letVariable = 'hello';
+if(true){
+let letVariable = 'hello1';
+var varVariable;
+//const constVariable = 42;
+varVariable = 'test'
+}
+const text = 'JavaScript';
+console.log(varVariable);
+console.log(typeof(number));
+console.log(math);
+
+console.log(greeting);
+console.log(letVariable[0]);
+console.log(letVariable[letVariable.length-1]); 
+console.log(fullName);
+console.log(text.slice (4, 10)); // accepts negative values
+//console.log(text.toUpperCase());
+console.log(text.substring (4, 10)); //do not accept negative values
---
+++ lesson4/operators.js
@@ -0,0 +1,23 @@
+var number = 5 
+var number = true 
+var number = undefined 
+var number = null
+
+const variable =5 !==5
+console.log(variable);
+
+// console.log(5 + 3); // addition
+// console.log(5 - 3); // subtraction
+// console.log(5 * 3); // multiplication
+// console.log(5 / 3); // division
+
+// console.log(5 ** 3); // exponentiation
+// console.log(5 + 3 * 2); // operator precedence
+// console.log(5 % 2); // остача від ділення
+// console.log(number ++); //збільшує на 1, але повертає старе значення
+// console.log(number); //нове значення повертається ТУТ, тобто при наступному виклику
+// console.log(++ number); //збільшує на 1 і зразу повертає нове значення
+
+// console.log(1 == '1'); //true
+// console.log(1 === '1'); //false //строге порівняння, перевіряє і тип і значення
+// console.log(1 != '1'); //false
