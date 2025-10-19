8590f3d632023f7b75c778cd209b45c36609fa7c
IrynaKichura <Iryna.Kichura@gmail.com>
2025-10-19 20:17:46 +0300

    Added task1

---
+++ lesson6hw/task1.js
@@ -0,0 +1,12 @@
+function area1 (length, width) {
+    return length * width;
+    }
+console.log(area1(10, 5));
+
+const area2 = function(length, width) {
+    return length * width;
+    }
+console.log(area2(6, 4));
+
+const area3 = (length, width) => length * width;
+console.log(area3(7, 3));
