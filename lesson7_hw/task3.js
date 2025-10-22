217b917468f3a7f809e700ccc4c1c97fe62de38f
IrynaKichura <Iryna.Kichura@gmail.com>
2025-10-22 21:37:53 +0300

    Added 2 files for task3

---
+++ lesson7_hw/task3.js
@@ -0,0 +1,24 @@
+function divide(numerator, denominator) {
+    if (denominator === 0) {
+        throw new Error('Denominator cannot be zero');
+    } else if (typeof numerator !== 'number' || typeof denominator !== 'number') {
+        throw new Error('Both numerator and denominator must be numbers');
+    } else 
+    return  numerator / denominator;
+}
+
+
+
+try {
+    divide(10, 0);
+} catch  {
+  console.log('Denominator cannot be zero');
+}
+try {
+     divide(10, '0');
+} catch  {
+  console.log('Both numerator and denominator must be numbers');
+}finally {
+    console.log('Work is done');
+}
+console.log(divide(6780, 90)); 
---
+++ lesson7_hw/task3_v2.js
@@ -0,0 +1,24 @@
+function divide(numerator, denominator) {
+  try {
+    if (denominator === 0) {
+      throw new Error('Denominator cannot be zero');
+    }
+  } catch {
+    console.log('Denominator cannot be zero');
+   }
+  try {
+    if (typeof numerator !== 'number' || typeof denominator !== 'number') {
+      throw new Error('Both numerator and denominator must be numbers');
+    }
+  } catch {
+    console.log('Both numerator and denominator must be numbers');
+  } try{
+   return numerator / denominator;
+  }
+  finally {
+    console.log('Work is done');
+  }
+  
+}
+
+console.log(divide(6780, 90));
