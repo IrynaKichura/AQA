84e942dbbcd0ac1225c73f084626258ddd5d612e
IrynaKichura <Iryna.Kichura@gmail.com>
2025-10-26 20:47:31 +0200

    Added 2 files for lesson8

---
+++ lesson8/references.js
@@ -0,0 +1,26 @@
+//примітивні типи створюють копію при присвоєнні (видає 1 1 3 1 )
+// let num1=1;
+// console.log(num1);
+// let num2=num1
+// console.log(num2);
+// num2=3;
+// console.log(num2);
+// console.log(num1);
+
+//референси типи при присвоєнні створюють посилання на той самий об'єкт (видає [1] [1] [3] [1] - бо його не змінювали)
+// let num1=[1];
+// console.log(num1);
+// let num2=num1
+// console.log(num2);
+// num2=[3];
+// console.log(num2);
+// console.log(num1);
+
+//референси типи при зміні об'єкта через іншу змінну змінюється об'єкт і для першої змінної, бо при переприсвоєнні ми скопіювали лиш посилання на обєкт, а не сам обєкт (видає [1] [1] [1,2] [1,2])
+let num1=[1];
+console.log(num1);
+let num2=num1
+console.log(num2);
+num2.push(2);
+console.log(num2);
+console.log(num1);
