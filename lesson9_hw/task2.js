9ebb67e80207e13ff98fbff126c6c3d23a70030e
IrynaKichura <Iryna.Kichura@gmail.com>
2025-10-28 19:50:50 +0200

    Added task2

---
+++ lesson9_hw/task2.js
@@ -0,0 +1,9 @@
+const book = {
+  title: 'Very interesting book',
+  author: 'Ben Smith',
+  year: 2025
+};
+const {title, author} = book
+console.log(title, author)
+console.log(title)
+console.log(author)
