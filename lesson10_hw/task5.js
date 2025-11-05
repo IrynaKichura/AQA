be06fd9a7ec1af31ea35b4c13d9cd0a77f31bed2
IrynaKichura <Iryna.Kichura@gmail.com>
2025-11-05 22:32:28 +0200

    Added task5

---
+++ lesson10_hw/task5.js
@@ -0,0 +1,25 @@
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+
+}
+class EBook extends Book {
+    constructor(title, author, year, format) {
+    super(title, author, year);
+    this.format = format;
+  }
+    static createEbook(obj, format) {
+    return  new EBook(obj.title, obj.author, obj.year, format)
+  }
+  };
+
+  const book = new Book("This Book", "Some Person", 2025);
+  console.log(book);
+  //book.format = "fb2"
+   //console.log(book);
+  //const ebook = {...book, format: "fb2"}
+  //console.log(ebook);
+  console.log(EBook.createEbook(book, "pdf"));
