2ab135a55b4786064345eecce24ae7a940dfbcc6
IrynaKichura <Iryna.Kichura@gmail.com>
2025-11-17 16:34:05 +0200

    Added new files

--- lesson14/myTest.test.js
+++ lesson14/myTest.test.js
@@ -13,13 +13,30 @@
 
 // }
 // });
+const axios = require('axios');
 
-// describe("api test", ()=>{
-// test('Axios get', async ()=>{
-//     const response = (
-//     await axios.get("https://jsonplaceholder.typicode.com/posts")
-// ).data;
-//     //console.log(response)
-//     expect(response[0].userId).toBe(2)
-// });
-// })
+describe('api test', () => {
+    test('Axios get', async () => {
+        const response = (
+            await axios.get('https://jsonplaceholder.typicode.com/posts')
+        ).data;
+        console.log(response[0]);
+        expect(response[0].userId).toBe(1);
+    });
+});
+
+describe('api test', () => {
+    test('Axios post', async () => {
+        const body = {
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        };
+        const response = 
+            await axios.post('https://jsonplaceholder.typicode.com/posts',
+        body);
+        console.log(response[0]);
+        expect(response.data.id).toBeDefined();
+        expect(response.status).toBe(201);
+    });
+});
